8ef23eb1acb4fef16d0ad8a5d89ab5b8d3b9fa16
zhangyaaoo <zhangyaaoo@gmail.com>
2019-11-17 22:47:56 +0800

    加入计数信号量测试代码

--- code/User/app.c
+++ code/User/app.c
@@ -6,14 +6,34 @@ uint32_t FlagTwo;
 uint32_t FlagThree;
 uint32_t FlagFour;
 
+Sem_t sem_one;
+Sem_t sem_two;
+Sem_t sem_three;
+
+uint32_t is_destroyed;
+
 void TaskOne(void *param)
 {
+    uint32_t ret = 0;
+    SemInit(&sem_one, 0, 10);
+    SemInit(&sem_two, 0, 10);
+    SemInit(&sem_three, 2, 10);
     while (1)
     {
-        FlagOne = 0;
-        TaskDelay(1);
-        FlagOne = 1;
-        TaskDelay(1);
+        if (0 == FlagOne)
+        {
+            FlagOne = 1;
+        }
+        else
+        {
+            FlagOne = 0;
+        }
+        printf("task one: waiting sem_one\r\n");
+        ret = SemWait(&sem_one, 0);
+        if (ErrorCode_NoError == ret)
+        {
+            printf("task one: has got sem_one\r\n");
+        }
     }
 }
 
@@ -22,29 +42,75 @@ void TaskTwo(void *param)
     while (1)
     {
         FlagTwo = 1;
-        TaskDelay(1);
+        TaskDelay(5);
+        printf("task two: notify sem_one\r\n");
+        SemNotify(&sem_one);
         FlagTwo = 0;
-        TaskDelay(1);
+        TaskDelay(5);
+        printf("task two: notify sem_one\r\n");
+        SemNotify(&sem_one);
     }
 }
 
 void TaskThree(void *param)
 {
+    uint32_t ret = 0;
     while (1)
     {
-        FlagThree = 1;
-        TaskDelay(2);
-        FlagThree = 0;
-        TaskDelay(2);
+        if (0 == FlagThree)
+        {
+            FlagThree = 1;
+        }
+        else
+        {
+            FlagThree = 0;
+        }
+
+        printf("task three: waiting sem_two, 2 ticks timeout\r\n");
+        ret = SemWait(&sem_two,2);
+        if (ErrorCode_Timeout == ret)
+        {
+            printf("task three: waiting sem_two, has timeout\r\n");
+        }
+
+        if (1 != is_destroyed)
+        {
+            printf("task three: waiting sem_three\r\n");
+            SemWait(&sem_three, 0);
+        }
+        else
+        {
+            printf("task three: sem_three has been destoryed\r\n");
+        }
     }
 }
 
 void TaskFour(void *param)
 {
+    SemInfo_t sem_info;
+
+    SemGetInfo(&sem_three, &sem_info);
+    printf("Count = %d\r\n", sem_info.Count);
+    printf("MaxConut = %d\r\n", sem_info.MaxCount);
+    printf("TaskCount = %d\r\n", sem_info.TaskCount);
+
+    TaskDelay(50);
+
+    SemGetInfo(&sem_three, &sem_info);
+    printf("Count = %d\r\n", sem_info.Count);
+    printf("MaxConut = %d\r\n", sem_info.MaxCount);
+    printf("TaskCount = %d\r\n", sem_info.TaskCount);
+    SemNoWaitGet(&sem_three);
+    SemGetInfo(&sem_three, &sem_info);
+    printf("Count = %d\r\n", sem_info.Count);
+    printf("MaxConut = %d\r\n", sem_info.MaxCount);
+    printf("TaskCount = %d\r\n", sem_info.TaskCount);
+    SemDestroy(&sem_three);
+    is_destroyed = 1;
     while (1)
     {
         printf("hello,world!\r\n");
-        TaskDelay(20);
+        TaskDelay(50);
     }
 }
 
